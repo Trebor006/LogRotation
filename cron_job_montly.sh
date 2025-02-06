f803c274596479f0038d636c1839a553e5018887
Juan Eulate <juan_a_eulate@hotmail.com>
2025-02-06 01:01:21 -0400

    android-windows-zookeeper backups

--- cron_job_montly.sh
+++ cron_job_montly.sh
@@ -15,3 +15,19 @@ mv "$openSSHBackupFile" ../logs/open-ssh/
 thunderbirdBackupFile="thunderbird-$(date +%Y%m%d-%H%M%S).tar.gz" 
 tar -czvf "$thunderbirdBackupFile" Thunderbird_2k.log
 mv "$thunderbirdBackupFile" ../logs/thunderbird/
+
+
+#android
+androidBackupFile="android-$(date +%Y%m%d-%H%M%S).tar.gz"
+tar -czvf "$androidBackupFile" Android_2k.log
+mv "$androidBackupFile" ../logs/android/
+
+#windows
+windowsBackupFile="windows-$(date +%Y%m%d-%H%M%S).tar.gz"
+tar -czvf "$windowsBackupFile" Windows_2k.log
+mv "$windowsBackupFile" ../logs/windows/
+
+#zookeeper
+zookeeperBackupFile="zookeeper-$(date +%Y%m%d-%H%M%S).tar.gz"
+tar -czvf "$zookeeperBackupFile" Zookeeper_2k.log
+mv "$zookeeperBackupFile" ../logs/zookeeper/
