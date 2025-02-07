c47c618e7e306e9325329d3dbfeb6dcc0b29c4ae
Robert Cabrera <clrobert.06@gmail.com>
2025-02-07 00:08:45 -0400

    update files

--- config_crons.sh
+++ config_crons.sh
@@ -3,34 +3,34 @@
 #----------------------------------------------------------------------------------------
 logsFile="$HOME/logRotation/execution-logs/executionlogs.log"
 
-# Definir el script a ejecutar diariamente!!
+# Definir el script a ejecutar diariamente
+
 daily_script="$HOME/logRotation/ssh-scripts/cron_job_daily.sh"
 
 # Asegurar que el script tenga permisos de ejecución
 chmod +x "$daily_script"
 
 # Agregar la tarea al crontab
-(crontab -l 2>/dev/null; echo "* * * * * $daily_script | awk '{print strftime("[%Y-%m-%d %H:%M:%S]"), "[PID " PROCINFO["pid"] "]", $0}' >> $logsFile 2>&1") | crontab -
+(crontab -l 2>/dev/null | grep -v "$daily_script"; echo "* * * * * echo \"[\$(date '+\%Y-\%m-\%d \%H:\%M:\%S')] [PID $$] Ejecutando: $daily_script\" >> $logsFile; $daily_script >> $logsFile 2>&1") | crontab -
 
 # Verificar que el cron job fue agregado correctamente
-echo "Cron job configurado para ejecutar $daily_script cada dia."
+echo "Cron job configurado para ejecutar $daily_script cada día."
 crontab -l
 
 #----------------------------------------------------------------------------------------
 
-# Definir el script a ejecutar mensualmente!!!
+# Definir el script a ejecutar mensualmente
+
 monthly_script="$HOME/logRotation/ssh-scripts/cron_job_monthly.sh"
 
 # Asegurar que el script tenga permisos de ejecución
 chmod +x "$monthly_script"
 
 # Agregar la tarea al crontab
-(crontab -l 2>/dev/null; echo "* * * * * $monthly_script | awk '{print strftime("[%Y-%m-%d %H:%M:%S]"), "[PID " PROCINFO["pid"] "]", $0}' >> $logsFile 2>&1") | crontab -
+(crontab -l 2>/dev/null | grep -v "$monthly_script"; echo "0 0 1 * * echo \"[\$(date '+\%Y-\%m-\%d \%H:\%M:\%S')] [PID $$] Ejecutando: $monthly_script\" >> $logsFile; $monthly_script >> $logsFile 2>&1") | crontab -
 
 # Verificar que el cron job fue agregado correctamente
 echo "Cron job configurado para ejecutar $monthly_script cada mes."
 crontab -l
 
-
 #----------------------------------------------------------------------------------------
-
